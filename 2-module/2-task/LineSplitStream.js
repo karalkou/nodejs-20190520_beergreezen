58a4792af2a83e058bc4f21d352a24afda5bf057
Yury Karalkou <Yury_Karalkou@epam.com>
2019-06-06 09:24:01 +0300

    Brush the code of solution for 2-module 2-task

--- 2-module/2-task/LineSplitStream.js
+++ 2-module/2-task/LineSplitStream.js
@@ -1,6 +1,5 @@
 const stream = require('stream');
 const os = require('os');
-// const fs = require('fs');
 
 class LineSplitStream extends stream.Transform {
   constructor(options) {
@@ -12,8 +11,13 @@ class LineSplitStream extends stream.Transform {
     const tail = this.tailStorage != null ? this.tailStorage : '';
     const chunkArr = (tail + chunk.toString()).split(`${os.EOL}`);
 
+    /**
+    * The last array item (data after the last completed line with os.EOL)
+    * is removed with Array.pop() and stored for future
+    */
     this.tailStorage = chunkArr.pop();
 
+    /* Push each chunkLine separately */
     chunkArr.forEach((chunkLine) => this.push(chunkLine));
 
     callback();
@@ -24,54 +28,5 @@ class LineSplitStream extends stream.Transform {
     callback();
   }
 }
-
-// --- my LineSplitStream experimen
-const lines = new LineSplitStream({encoding: 'utf-8'});
-
-lines.on('data', (chunk) => {
-  console.log('---lines data: ', chunk.toString());
-});
-
-lines.on('end', () => {
-  console.log('---lines end');
-});
-
-lines.on('finish', () => {
-  console.log('---lines finish');
-});
-lines.on('close', () => {
-  console.log('---lines close');
-});
-
-// fs.createReadStream('line.txt').pipe(lines);
-lines.write('нулевая');
-lines.write(`первая строка${os.EOL}вторая строка${os.EOL}третья строка`);
-lines.end();
-// lines.destroy();
-
-
-// --------------------------------------------------------
-
-// --- my readable experiment
-/* const myReadStream = fs.createReadStream('line.txt'); */
-
-/* myReadStream.on('readable', () => {
-  console.log('--myReadStream-- readable');
-  let chunk = null;
-
-  while(null !== (chunk = myReadStream.read(5))) {
-      console.log('-myReadStream-- readable chunk.toString(): ', chunk.toString());
-  }
-}); */
-/* myReadStream.on('data', (chunk) => {
-  console.log('--myReadStream-- data: ', chunk.toString());
-});
-myReadStream.on('end', () => {
-  console.log('--myReadStream-- end');
-});
-myReadStream.on('close', () => {
-  console.log('--myReadStream close');
-}); */
-
 
 module.exports = LineSplitStream;
