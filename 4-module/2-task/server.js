b02d0d174e02a90b6a737e6c2503137051176be3
Yury Karalkou <Yury_Karalkou@epam.com>
2019-06-10 09:47:46 +0300

    Add solution for 4-module 2-task

--- 4-module/2-task/server.js
+++ 4-module/2-task/server.js
@@ -1,22 +1,81 @@
 const url = require('url');
 const http = require('http');
 const path = require('path');
+const fs = require('fs');
+const LimitSizeStream = require('./LimitSizeStream');
 
 const server = new http.Server();
 
 server.on('request', (req, res) => {
+  req.on('error', (err) => {
+    console.log('server request error: ', err);
+  });
+
+  res.on('error', (err) => {
+    console.log('server response error: ', err);
+  });
+
   const pathname = url.parse(req.url).pathname.slice(1);
 
   const filepath = path.join(__dirname, 'files', pathname);
 
   switch (req.method) {
-    case 'POST':
+    case 'POST': {
+      if (pathname.includes('/')) {
+        res.statusCode = 400;
+        res.statusMessage = 'Server doesn\'t support nested paths';
+        res.end();
+      }
+
+      const writeStream = fs.createWriteStream(filepath, {flags: 'wx'});
+      const limitStream = new LimitSizeStream({limit: 1000000});
+
+      req
+          .pipe(limitStream)
+          .on('error', (err) => {
+            if (err.code === 'LIMIT_EXCEEDED') {
+              res.statusCode = 413;
+              res.statusMessage = 'Limit is exceeded!';
+
+              fs.unlink(filepath, (err) => {});
+
+              res.end();
+            }
+          })
+          .pipe(writeStream)
+          .on('error', (err) => {
+            if (err.code === 'EEXIST') {
+              res.statusCode = 409;
+              res.statusMessage = 'File already exists!';
+              res.end();
+            } else {
+              res.statusCode = 500;
+              res.statusMessage = 'Internal server error';
+              res.end();
+            }
+          })
+          .on('close', () => {
+            res.statusCode = 201;
+            res.statusMessage = 'Successfully created';
+            res.end();
+          });
+
+      // remove file from disc if writing was interrupted
+      res
+          .on('close', () => {
+            if (res.finished) {
+              return null;
+            }
+
+            fs.unlink(filepath, (err) => {});
+          });
 
       break;
-
-    default:
+    }
+    default: {
       res.statusCode = 501;
       res.end('Not implemented');
+    }
   }
 });
 
