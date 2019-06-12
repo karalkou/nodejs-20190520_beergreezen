0537856878fd6966b0267d493436edccf8553de8
Yury Karalkou <Yury_Karalkou@epam.com>
2019-06-12 20:35:09 +0300

    Add solution for 4-module 3-task

--- 4-module/3-task/server.js
+++ 4-module/3-task/server.js
@@ -1,22 +1,47 @@
 const url = require('url');
 const http = require('http');
 const path = require('path');
+const fs = require('fs');
 
 const server = new http.Server();
 
 server.on('request', (req, res) => {
   const pathname = url.parse(req.url).pathname.slice(1);
-
   const filepath = path.join(__dirname, 'files', pathname);
 
   switch (req.method) {
-    case 'DELETE':
+    case 'DELETE': {
+      if (pathname.includes('/')) {
+        res.statusCode = 400;
+        res.statusMessage = 'Server doesn\'t support nested paths';
+        res.end();
+      }
 
+      fs.unlink(filepath, (err) => {
+        if (err) {
+          switch (err.code) {
+            case 'ENOENT': {
+              res.statusCode = 404;
+              res.end();
+              break;
+            }
+            default: {
+              res.statusCode = 500;
+              res.end();
+            }
+          }
+        }
+
+        res.statusCode = 200;
+        res.statusMessage = 'Ok';
+        res.end();
+      });
       break;
-
-    default:
+    }
+    default: {
       res.statusCode = 501;
       res.end('Not implemented');
+    }
   }
 });
 
