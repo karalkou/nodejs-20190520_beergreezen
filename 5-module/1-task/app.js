98be00c46ef7508f630287385f8631f8b7433b60
Yury Karalkou <Yury_Karalkou@epam.com>
2019-06-13 09:00:25 +0300

    Prepare 5-module 1-task for work on solution

--- 5-module/1-task/app.js
+++ 5-module/1-task/app.js
@@ -1,16 +1,29 @@
 const Koa = require('koa');
 const app = new Koa();
+const path = require('path');
 
-app.use(require('koa-static')('public'));
+const pathToStatic = path.join(__dirname, 'public');
+
+app.use(require('koa-static')(pathToStatic));
 app.use(require('koa-bodyparser')());
 
 const Router = require('koa-router');
 const router = new Router();
 
+router.get('/kys', async (ctx, next) => {
+  console.log('kys');
+  ctx.body = 'kys-kys';
+  return next();
+});
+
 router.get('/subscribe', async (ctx, next) => {
+  console.log('Subscribe');
+  return next();
 });
 
 router.post('/publish', async (ctx, next) => {
+  console.log('Publish');
+  return next();
 });
 
 app.use(router.routes());
--- 5-module/1-task/index.js
+++ 5-module/1-task/index.js
@@ -1,5 +1,6 @@
 const app = require('./app');
+const port = 3000;
 
-app.listen(3000, () => {
-  console.log('App is running on http://localhost:3000');
+app.listen(port, () => {
+  console.log(`App is running on http://localhost:${port}`);
 });
