38966bc55c493b749a50c4d1dd63cc3369923fe6
Yury Karalkou <Yury_Karalkou@epam.com>
2019-06-13 20:16:48 +0300

    Add solution of 5-module 1-task

--- 5-module/1-task/app.js
+++ 5-module/1-task/app.js
@@ -10,20 +10,36 @@ app.use(require('koa-bodyparser')());
 const Router = require('koa-router');
 const router = new Router();
 
-router.get('/kys', async (ctx, next) => {
-  console.log('kys');
-  ctx.body = 'kys-kys';
-  return next();
-});
+let clients = [];
 
 router.get('/subscribe', async (ctx, next) => {
-  console.log('Subscribe');
-  return next();
+  const message = await new Promise((resolve, reject) => {
+    clients.push(resolve);
+
+    ctx.res.on('close', function() {
+      clients.pop(resolve);
+      resolve();
+    });
+  });
+
+  ctx.body = message;
 });
 
 router.post('/publish', async (ctx, next) => {
-  console.log('Publish');
-  return next();
+
+  const {message} = ctx.request.body;
+
+  if (!message) {
+    ctx.throw(400, 'Bad request! Field `message` is absent');
+  }
+
+  clients.forEach(function(resolve) {
+    resolve(message);
+  });
+
+  clients = [];
+
+  ctx.body = 'ok';
 });
 
 app.use(router.routes());
